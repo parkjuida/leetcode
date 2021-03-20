6d0320cb1580d44c4b5a3529ddd52c42a20c7472
parkjuida <bees1114@naver.com>
2021-03-21 01:14:58 +0900

    Rename main

--- java/Main.java
+++ java/Main.java
@@ -3,8 +3,16 @@
 class Main {
     public static void main(String[] args) {
         LetterCombinationsOfAPhoneNumber l = new LetterCombinationsOfAPhoneNumber();
-//        l.solve();
-        Sum4 sum4 = new Sum4();
-        sum4.solve();
+        System.out.println(l.letterCombinations("23"));
+        System.out.println(l.letterCombinations("293"));
+        System.out.println(l.letterCombinations("2222"));
+        System.out.println(l.letterCombinations("2"));
+        System.out.println(l.letterCombinations("3"));
+        System.out.println(l.letterCombinations("4"));
+        System.out.println(l.letterCombinations("5"));
+        System.out.println(l.letterCombinations("6"));
+        System.out.println(l.letterCombinations("7"));
+        System.out.println(l.letterCombinations("8"));
+        System.out.println(l.letterCombinations("9"));
     }
 }
