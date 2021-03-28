0ae7b908553e985d55b9b04bba8c2edb41a7988e
parkjuida <bees1114@naver.com>
2021-03-28 21:40:54 +0900

    solve MergeTwoSortedLists in java

--- java/Main.java
+++ java/Main.java
@@ -2,17 +2,9 @@
 
 class Main {
     public static void main(String[] args) {
-        LetterCombinationsOfAPhoneNumber l = new LetterCombinationsOfAPhoneNumber();
-        System.out.println(l.letterCombinations("23"));
-        System.out.println(l.letterCombinations("293"));
-        System.out.println(l.letterCombinations("2222"));
-        System.out.println(l.letterCombinations("2"));
-        System.out.println(l.letterCombinations("3"));
-        System.out.println(l.letterCombinations("4"));
-        System.out.println(l.letterCombinations("5"));
-        System.out.println(l.letterCombinations("6"));
-        System.out.println(l.letterCombinations("7"));
-        System.out.println(l.letterCombinations("8"));
-        System.out.println(l.letterCombinations("9"));
+
+        StrStr str = new StrStr();
+        int ret = str.strStr("mississippi", "issip");
+        System.out.println(ret);
     }
 }
--- java/MergeTwoSortedLists.java
+++ java/MergeTwoSortedLists.java
@@ -1,62 +1,62 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode res = null;
-        ListNode tempLast = null;
-        int thisVal = 0;
-
-        while(l1 != null && l2 != null) {
-            if(l1.val > l2.val) {
-                if(res == null) {
-                    res = l2;
-                    tempLast = res;
-                } else {
-                    tempLast.next = l2;
-                    tempLast = tempLast.next;
-                }
-                l2 = l2.next;
-            } else {
-                if(res == null) {
-                    res = l1;
-                    tempLast = res;
-                } else {
-                    tempLast.next = l1;
-                    tempLast = tempLast.next;
-                }
-                l1 = l1.next;
-            }
-        }
-
-        while(l1 != null) {
-            if(res == null) {
-                res = l1;
-                tempLast = res;
-            } else {
-                tempLast.next = l1;
-                tempLast = tempLast.next;
-            }
-            l1 = l1.next;
-        }
-        while(l2 != null) {
-            if(res == null) {
-                res = l2;
-                tempLast = res;
-            } else {
-                tempLast.next = l2;
-                tempLast = tempLast.next;
-            }
-            l2 = l2.next;
-        }
-
-        return res;
-    }
-}
+///**
+// * Definition for singly-linked list.
+// * public class ListNode {
+// *     int val;
+// *     ListNode next;
+// *     ListNode() {}
+// *     ListNode(int val) { this.val = val; }
+// *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// * }
+// */
+//class Solution {
+//    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//        ListNode res = null;
+//        ListNode tempLast = null;
+//        int thisVal = 0;
+//
+//        while(l1 != null && l2 != null) {
+//            if(l1.val > l2.val) {
+//                if(res == null) {
+//                    res = l2;
+//                    tempLast = res;
+//                } else {
+//                    tempLast.next = l2;
+//                    tempLast = tempLast.next;
+//                }
+//                l2 = l2.next;
+//            } else {
+//                if(res == null) {
+//                    res = l1;
+//                    tempLast = res;
+//                } else {
+//                    tempLast.next = l1;
+//                    tempLast = tempLast.next;
+//                }
+//                l1 = l1.next;
+//            }
+//        }
+//
+//        while(l1 != null) {
+//            if(res == null) {
+//                res = l1;
+//                tempLast = res;
+//            } else {
+//                tempLast.next = l1;
+//                tempLast = tempLast.next;
+//            }
+//            l1 = l1.next;
+//        }
+//        while(l2 != null) {
+//            if(res == null) {
+//                res = l2;
+//                tempLast = res;
+//            } else {
+//                tempLast.next = l2;
+//                tempLast = tempLast.next;
+//            }
+//            l2 = l2.next;
+//        }
+//
+//        return res;
+//    }
+//}
