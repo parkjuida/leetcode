f1b15f9bafa813ff12be854952ef2c635a84a2f3
parkjuida <bees1114@naver.com>
2021-09-13 21:49:09 +0900

    Resolve climbing_stairs in Python

--- python/climbing_stairs.py
+++ python/climbing_stairs.py
@@ -1,18 +1,21 @@
 class Solution:
-    def climb(self, n, history):
-        print(n)
+    def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
         if n == 2:
             return 2
-        try:
-            ret = history[n]
-        except KeyError:
-            ret = self.climb(n - 1, history) + self.climb(n - 2, history)
-            history[n] = ret
 
-        return ret
+        prev = 2
+        prev_prev = 1
+        for i in range(3, n + 1):
+            temp = prev
+            prev = prev + prev_prev
+            prev_prev = temp
 
-    def climbStairs(self, n: int) -> int:
-        history = {}
-        return self.climb(n, history)
+        return prev
+
+
+s = Solution()
+print(s.climbStairs(3))
+print(s.climbStairs(4))
+print(s.climbStairs(5))
