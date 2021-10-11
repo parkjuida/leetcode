5bfd33ab02b91bfc6058b3d6d3bb37ae381782fe
parkjuida <bees1114@naver.com>
2021-10-11 15:55:30 +0900

    Improve speed for finding blocks

--- python/rotating_the_box.py
+++ python/rotating_the_box.py
@@ -6,12 +6,17 @@ class Solution:
         answer = [[box[len(box) - j - 1][i] for j in range(len(box))] for i in range(len(box[0]))]
 
         for i in range(len(answer[0])):
-            for j in range(len(answer) - 1, -1, -1):
-                t = j
+            t = j = len(answer) - 1
+            while t > -1:
                 while t - 1 > -1 and answer[t][i] == '.':
                     t -= 1
                 if t > -1 and answer[t][i] == '#':
                     answer[t][i], answer[j][i] = answer[j][i], answer[t][i]
+                    j -= 1
+                if t > -1 and answer[t][i] == '*':
+                    j = t - 1
+
+                t -= 1
 
         return answer
 
